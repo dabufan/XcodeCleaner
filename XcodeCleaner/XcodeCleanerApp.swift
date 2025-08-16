ead4246aa61a454d758add8425a5881d96bcdb97
dudu <dabufan@gmail.com>
2025-08-17 00:53:56 +0800

    Initial Commit

---
+++ XcodeCleaner/XcodeCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCleanerApp.swift
+//  XcodeCleaner
+//
+//  Created by Q Lee on 2025/8/17.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
